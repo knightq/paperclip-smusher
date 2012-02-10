3a5e1a61a41dbb57b37a05dbe2cb1efbe8399801
Andrea Salicetti <andrea.salicetti@gmail.com>
2012-02-10 14:35:03 +0100

    Updates.

--- lib/paperclip-smusher.rb
+++ lib/paperclip-smusher.rb
@@ -1,51 +1,53 @@
 module Paperclip
   class Thumbnail < Processor
+
+    # Performs the conversion of the +file+ into a thumbnail. Returns the Tempfile
+    # that contains the new image.
     def make
-    src = @file
-    conv = Tempfile.new([@basename, @format ? ".#{@format}" : ''])
-    conv.binmode
-    dst = Tempfile.new([@basename, @format ? ".#{@format}" : ''])
-    dst.binmode
+      src = @file
+      conv = Tempfile.new([@basename, @format ? ".#{@format}" : ''])
+      conv.binmode
+      dst = Tempfile.new([@basename, @format ? ".#{@format}" : ''])
+      dst.binmode
 
-  begin
-    parameters = []
-    parameters << source_file_options
-    parameters << ":source"
-    parameters << transformation_command
-    parameters << convert_options
-    parameters << ":dest"
+      begin
+        parameters = []
+        parameters << source_file_options
+        parameters << ":source"
+        parameters << transformation_command
+        parameters << convert_options
+        parameters << ":dest"
 
-    parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
+        parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
 
-    success = Paperclip.run("convert", parameters, :source => "#{File.expand_path(src.path)}[0]", :dest => File.expand_path(conv.path))
+        success = Paperclip.run("convert", parameters, :source => "#{File.expand_path(src.path)}#{'[0]' unless animated?}", :dest => File.expand_path(conv.path))
+        if conv.size > 0
+          format = begin
+            Paperclip.run("identify", "-format %m :file", :file => "#{File.expand_path(conv.path)}[0]")
+          rescue PaperclipCommandLineError
+            ""
+          end
 
-    if conv.size > 0
-      format = begin
-        Paperclip.run("identify", "-format %m :file", :file => "#{File.expand_path(conv.path)}[0]")
-      rescue PaperclipCommandLineError
-        ""
+          case format.strip
+          when 'JPEG'
+            # Part of libjpeg-progs deb package
+            success = Paperclip.run('jpegtran', "-copy none -optimize -perfect :source > :dest", :source => File.expand_path(conv.path), :dest => File.expand_path(dst.path))
+          when 'PNG'
+            success = Paperclip.run('pngcrush', "-rem alla -reduce -brute :source :dest", :source => File.expand_path(conv.path), :dest => File.expand_path(dst.path))
+          else
+            dst = conv
+          end
+        end
+      rescue Cocaine::ExitStatusError => e
+        raise PaperclipError, "There was an error processing the thumbnail for #{@basename}" if @whiny
+      rescue Cocaine::CommandNotFoundError => e
+        raise Paperclip::CommandNotFoundError.new("Could not run the `convert` command. Please install ImageMagick.")
+      rescue Exception => e
+        Rails.logger.error "There was an error processing the thumbnail for #{@basename}. Check imagemagick, jpegtran and pngcrush installed."
+        HoptoadNotifier.notify(:error_class => "Paperclip - images optimization", :error_message => "Paperclip ERROR: #{e.message}")
       end
 
-      case format.strip
-      when 'JPEG'
-        # Part of libjpeg-progs deb package
-        success = Paperclip.run('jpegtran', "-copy none -optimize -perfect :source > :dest", :source => File.expand_path(conv.path), :dest => File.expand_path(dst.path))
-      when 'PNG'
-        success = Paperclip.run('pngcrush', "-rem alla -reduce -brute :source :dest", :source => File.expand_path(conv.path), :dest => File.expand_path(dst.path))
-      else
-        dst = conv
-      end
-    end
-  rescue Exception => e
-    Rails.logger.error "There was an error processing the thumbnail for #{@basename}. Check imagemagick, jpegtran and pngcrush installed."
-
-    HoptoadNotifier.notify(
-      :error_class => "Paperclip - images optimization",
-      :error_message => "Paperclip ERROR: #{e.message}"
-    )
-  end
-
-  dst.size > 0 ? dst : conv
+      dst.size > 0 ? dst : conv
     end
   end
 end
