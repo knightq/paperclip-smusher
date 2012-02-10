43061c894f2e84bb7ba3e80a17dc06cba11a7cb9
Andrea Salicetti <andrea.salicetti@gmail.com>
2012-02-10 14:14:00 +0100

    First release

---
+++ lib/paperclip-smusher.rb
@@ -0,0 +1,51 @@
+module Paperclip
+  class Thumbnail < Processor
+    def make
+    src = @file
+    conv = Tempfile.new([@basename, @format ? ".#{@format}" : ''])
+    conv.binmode
+    dst = Tempfile.new([@basename, @format ? ".#{@format}" : ''])
+    dst.binmode
+
+  begin
+    parameters = []
+    parameters << source_file_options
+    parameters << ":source"
+    parameters << transformation_command
+    parameters << convert_options
+    parameters << ":dest"
+
+    parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
+
+    success = Paperclip.run("convert", parameters, :source => "#{File.expand_path(src.path)}[0]", :dest => File.expand_path(conv.path))
+
+    if conv.size > 0
+      format = begin
+        Paperclip.run("identify", "-format %m :file", :file => "#{File.expand_path(conv.path)}[0]")
+      rescue PaperclipCommandLineError
+        ""
+      end
+
+      case format.strip
+      when 'JPEG'
+        # Part of libjpeg-progs deb package
+        success = Paperclip.run('jpegtran', "-copy none -optimize -perfect :source > :dest", :source => File.expand_path(conv.path), :dest => File.expand_path(dst.path))
+      when 'PNG'
+        success = Paperclip.run('pngcrush', "-rem alla -reduce -brute :source :dest", :source => File.expand_path(conv.path), :dest => File.expand_path(dst.path))
+      else
+        dst = conv
+      end
+    end
+  rescue Exception => e
+    Rails.logger.error "There was an error processing the thumbnail for #{@basename}. Check imagemagick, jpegtran and pngcrush installed."
+
+    HoptoadNotifier.notify(
+      :error_class => "Paperclip - images optimization",
+      :error_message => "Paperclip ERROR: #{e.message}"
+    )
+  end
+
+  dst.size > 0 ? dst : conv
+    end
+  end
+end
---
+++ paperclip-smusher.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name        = "paperclip-smusher"
+  s.version     = "0.1.0"
+  s.date        = "2012-02-10"
+  s.summary     = "Image compression for Paperclip"
+  s.description = "JPEG and PNG compression for Paperclip gem"
+  s.author      = "Andrea Salicetti"
+  s.email       = "andrea.salicetti@gmail.com"
+  s.files       = ["lib/paperclip-smusher.rb"]
+  s.homepage    = "http://github.com/knightq/paperclip-smusher"
+  s.add_runtime_dependency "paperclip", ["~> 2.4"]
+  s.requirements << "jpegtran for JPEG compression"
+  s.requirements << "optipng for PNG compression"
+end
